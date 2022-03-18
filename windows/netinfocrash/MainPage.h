5c9af913db8c8e3a957cd133d1e83886a67d924f
Aaron Gifford <aaron.gifford1@hp.com>
2022-03-18 15:20:49 -0700

    Initial commit

---
+++ windows/netinfocrash/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::netinfocrash::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::netinfocrash::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
